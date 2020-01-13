0ab4a7c9a9649356b394b6d61faf29164686c5c5
salvatorelomonaco <lommetto95@gmail.com>
2020-01-13 19:30:14 +0100

    Method GET/POST/DELETE

--- main.js
+++ main.js
@@ -6,14 +6,47 @@ $(document).ready(function() {
 
     $('button').click(function() {
         var nuovoTodo = $('input').val();
-        aggiuntaList(nuovoTodo);
+        if (nuovoTodo.length > 0) {
+            aggiuntaList(nuovoTodo);
+            nuovoTodo = $('input').val('');
+        } else {
+            alert('Insert a valid task.')
+        }
+    });
+
+    $('input').keypress(function(event) {
+        if (event.which == 13) {
+            var nuovoTodo = $('input').val().trim();
+            if (nuovoTodo.length > 0) {
+                 $('input').val('');
+                aggiuntaList(nuovoTodo);
+            } else {
+                alert('Insert a valid task.')
+            }
+        }
+    });
+
+    $('#list').on('click', '.delete', function(){
+        var idToDoCurrent = $(this).parent().attr('data-id');
+        $.ajax({
+            'url': 'http://157.230.17.132:3011/todos/' + idToDoCurrent,
+            'method': 'DELETE',
+            'success': function(data) {
+                stampaList();
+            },
+            'error': function(data) {
+                alert('Error');
+            }
+        });
     });
 
     function aggiuntaList(text) {
-        $('#list').empty();
         $.ajax({
             'url': 'http://157.230.17.132:3011/todos',
             'method': 'POST',
+            'data': {
+                'text': text
+            },
             'success': function(data) {
                 stampaList();
             },
@@ -25,9 +58,10 @@ $(document).ready(function() {
 
     function stampaList() {
         $.ajax({
-            'url': 'http://157.230.17.132:3011/todos',
+            'url': 'http://157.230.17.132:3011/todos/',
             'method': 'GET',
             'success': function(data) {
+                $('#list').empty();
                 var toDos = data
                 for (var i = 0; i < toDos.length; i++) {
                     var toDoText = toDos[i].text;
